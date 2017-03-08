99335dc131df1b030415526a3348cf68a7c7dcaf
dvlprliu <dvlprliu@gmail.com>
2017-03-08 22:12:42 +0800

    update

---
+++ ch4/calculator.c
@@ -0,0 +1,90 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+
+int getop(char[]);
+void push(double);
+double pop(void);
+
+int main() {
+  int type;
+  double op2;
+  char s[MAXOP];
+
+  while ((type = getop(s)) != EOF) {
+    switch (type) {
+    case NUMBER:
+      push(atof(s));
+      break;
+    case '+':
+      push(pop() + pop());
+      break;
+    case '*':
+      push(pop() * pop());
+      break;
+    case '/':
+      op2 = pop();
+      if (op2 != 0.0) {
+        push(pop() / op2);
+      } else {
+        printf("error: zero diviser \n");
+      }
+      break;
+    case '-':
+      op2 = pop();
+      push(pop() - op2);
+      break;
+    case '\n':
+      printf("\t%.8g\n", pop());
+      break;
+    default:
+      printf("error: unknown command %s\n", s);
+      break;
+    }
+  }
+}
+
+#define MAXVAL 100
+
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f) {
+  if (sp < MAXVAL) {
+    val[sp++] = f;
+  } else {
+    printf("error: stack full, can't push %g\n", f);
+  }
+}
+
+double pop(void) {
+  if (sp > 0)
+    return val[--sp];
+  else {
+    printf("error: stack empty");
+    return 0.0;
+  }
+}
+
+int getop(char s[]) {
+  int i, c;
+  while ((s[0] = c = getch()) == ' ' || c == '\t')
+    ;
+  s[1] = '\0';
+  if (!isdigit(c) && c != '.')
+    return c;
+  i = 0;
+  if (isdigit(c))
+    while (isdigit(s[++i] = c = getchar()))
+      ;
+  if (c == '.')
+    while (isdigit(s[++i] = c = getch()))
+      ;
+  s[i] = '\0';
+  if (c != EOF)
+    ungetc(c);
+  return NUMBER;
+}
